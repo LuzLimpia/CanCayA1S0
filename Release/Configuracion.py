4c8385d21d32a5b03e9de9587b3968a9d02d88e3
LuzLimpia <84425590+LuzLimpia@users.noreply.github.com>
2021-06-05 19:38:15 -0500

    Update Configuracion.py

--- Release/Configuracion.py
+++ Release/Configuracion.py
@@ -8,8 +8,8 @@ EnvioAlaNube = True # Define si los datos serán enviados a Internet a traves de
 TxRxPins = [33, 19] # Los pines donde está conectado el sensor [33, 19] para cancay a1,
 LogEnTerminal = False # Habilita el envío de log a la consola repl
 MQTT_PORT = 1883 # Parametros para el MQTT
-MQTT_TOPIC = "cancay"
-MQTT_HOST = "ec2-3-83-89-138.compute-1.amazonaws.com"
+MQTT_TOPIC = "CanCayTopic"
+MQTT_HOST = "afcmd3bikg7hg-ats.iot.us-east-1.amazonaws.com"
 OTA_Files = ["Configuracion.py"] # Aquí se definen los archivos que formarán parte de la actualizacion OTA (Over The Air)
 
 # Esta es solo una linea para probar el OTA
